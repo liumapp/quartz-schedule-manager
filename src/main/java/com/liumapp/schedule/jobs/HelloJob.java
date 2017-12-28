ae6285e8d8c6fe6bd45f0ac4dc0194f5e0639ba0
liumapp <liumapp.com@gmail.com>
2017-12-28 09:54:57 +0800

    add jobs demo

---
+++ src/main/java/com/liumapp/schedule/jobs/HelloJob.java
@@ -0,0 +1,35 @@
+package com.liumapp.schedule.jobs;
+
+import com.liumapp.schedule.service.HelloService;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * Created by liumapp on 12/28/17.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+@Component
+public class HelloJob implements Job {
+
+
+    @Autowired
+    HelloService helloService;
+
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+        String jobName = jobExecutionContext.getJobDetail().getKey().getName();
+        System.out.println("jobName =" + jobName);
+        Map<String, Object> parameter = jobExecutionContext.getMergedJobDataMap();
+        System.out.println("Job execute result :" + helloService.sayHello(parameter));
+        System.out.println(new Date() + ": doing other things..." + Thread.currentThread().getId());
+    }
+}
