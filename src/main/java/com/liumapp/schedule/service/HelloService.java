fd29e434ddd7fbc3c41bda2b82e85c1c24657113
liumapp <liumapp.com@gmail.com>
2018-01-04 10:48:07 +0800

    use service

---
+++ src/main/java/com/liumapp/schedule/service/HelloService.java
@@ -0,0 +1,13 @@
+package com.liumapp.schedule.service;
+
+import java.util.Map;
+
+/**
+ * Created by liumapp on 1/4/18.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+public interface HelloService {
+    String sayHello(Map<String, Object> parameter);
+}
+
